6264efc1bfea17fe170c1f50f8764f2def9efe8b
serhii_butkaliuk <serhii.butkaliuk@mheducation.com>
2025-04-30 13:59:46 +0200

    update to 11

--- src/app/components/add-todo/add-todo.component.spec.ts
+++ src/app/components/add-todo/add-todo.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AddTodoComponent } from './add-todo.component';
 
@@ -6,7 +6,7 @@ describe('AddTodoComponent', () => {
   let component: AddTodoComponent;
   let fixture: ComponentFixture<AddTodoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AddTodoComponent ]
     })
