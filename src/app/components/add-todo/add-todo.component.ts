857a91a72505cc8079fb625ba264464e940d2f71
serhii_butkaliuk <serhii.butkaliuk@mheducation.com>
2025-04-30 14:48:27 +0200

    Hacking plan

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class AddTodoComponent implements OnInit {
   @Output() addTodo: EventEmitter<any> = new EventEmitter();
-  title: string;
+  title: string = '';
 
   constructor() { }
 
@@ -20,6 +20,7 @@ export class AddTodoComponent implements OnInit {
       completed: false
     }
     this.addTodo.emit(todo);
+    this.title = '';
   }
 
 }
